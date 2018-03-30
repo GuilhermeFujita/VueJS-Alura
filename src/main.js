3455a20498a861d417565ab56a8b4c8cf1d27f73
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-03-30 15:09:16 -0300

    Baixando, importando e usando VueResource

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//Importando e usando VueResource 
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+
 new Vue({
   el: '#app',
   render: h => h(App)
