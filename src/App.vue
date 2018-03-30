93ad8d1522d21491327b01660301ef95617e9f04
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-03-30 15:36:48 -0300

    Pegando fotos do servidor via requsição http

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,14 @@ export default {
     },
 
     created(){
-        alert("Criado o componente");
+        /**
+         * Faz a requisição que é uma promise
+         * Pede os dados em json que também é uma promise
+         * Recebe os dados no último then e joga no array fotos do objeto data
+         */
+        this.$http.get('http://localhost:3000/v1/fotos')
+        .then(res => res.json())
+        .then(fotos => this.fotos = fotos, err => console.log(err));
     }
 }
 
