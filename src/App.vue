95cbc53f777b9c7173790235ef15d4c3f9ab1e1e
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-03-30 14:34:41 -0300

    Recebendo dados da propriedade data e preenchendo template

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
-        <h1>Alurapic</h1>
-        <img src="http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg" alt="Cachorro">
+        <h1>{{ titulo }}</h1>
+        <img :src="foto.url" :alt="foto.titulo">
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+
+    data(){
+
+        return {
+            titulo: 'Alurapic',
+            foto: {
+                url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
+                titulo: 'Cachorro'
+            }
+        }
+    }
+}
+
 </script>
 
 <style>
